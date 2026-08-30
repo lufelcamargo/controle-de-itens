e23f6e8bae5fc3f333279a997c239ca12cf79d22
lufelcamargo <lipe311973@gmail.com>
2026-08-30 17:53:32 -0300

    add: navigation composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,5 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.navigation.compose)
 }
