9c5fcde37d1ba5536c5c60f8ed2f124955adcf61
lufelcamargo <lipe311973@gmail.com>
2026-09-06 11:46:54 -0300

    add: definição da paleta de cores do aplicativo após design definido

--- app/src/main/java/com/example/controleitens/ui/theme/Theme.kt
+++ app/src/main/java/com/example/controleitens/ui/theme/Theme.kt
@@ -1,17 +1,36 @@
 package com.example.controleitens.ui.theme
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val LightColorScheme = lightColorScheme()
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFFCBA6F7),
+    onPrimary = Color(0xFF1E1E2E),
+
+    primaryContainer = Color(0xFF45475A),
+    onPrimaryContainer = Color(0xFFCDD6F4),
+
+    background = Color(0xFF1E1E2E),
+    onBackground = Color(0xFFCDD6F4),
+
+    surface = Color(0xFF181825),
+    onSurface = Color(0xFFCDD6F4),
+
+    surfaceVariant = Color(0xFF313244),
+    onSurfaceVariant = Color(0xFFA6ADC8),
+
+    error = Color(0xFFF38BA8),
+    onError = Color(0xFF1E1E2E)
+)
 
 @Composable
 fun ControleItensTheme(
     content: @Composable () -> Unit
 ) {
     MaterialTheme(
-        colorScheme = LightColorScheme,
+        colorScheme = DarkColorScheme,
         content = content
     )
 }
